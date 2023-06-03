6292e568bf620e12db4dbce952d9769056ec545f
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-03 23:26:37 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../vendor/autoload.php'; // include Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php'; // include Composer's autoloader
 
 $client = new MongoDB\Client("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
 $collection = $client->demo->beers;
