da37b62a44f4d81b45cd7e3df6fedaff41c4912d
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-03 23:15:24 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php
-
+<?php 
 require 'vendor/autoload.php'; // include Composer's autoloader
 
 $client = new MongoDB\Client("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
-$collection = $client->demo->beers;
+$database = $client->foodMap;
 
-$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+$result = $database->restaurant->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
 
 echo "Inserted with Object ID '{$result->getInsertedId()}'";
 ?>
