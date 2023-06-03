e990b560181349cd211c671ac164cc5bfeadcd6a
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-04 02:09:25 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,11 @@
 <?php
-use MongoDB\Driver\Manager;
-use MongoDB\Driver\Query;
-// 建立 MongoDB 連接
-$manager = new Manager("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
-var_dump($manager);
+use MongoDB\Driver\Client;
 
-$filter = ['name' => 'Hinterland'];
+$client = new Client("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
+$database = $client->test;
 
-$query = new MongoDB\Driver\Query($filter);
-$cursor = $manager->executeQuery('foodMap.restaurant', $query); // $mongo contains the connection object to MongoDB
-foreach ($cursor as $document) {
-    var_dump($document);
-}
+$result = $database->restaurant->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+echo "Inserted with Object ID '{$result->getInsertedId()}'";
+
 ?>
