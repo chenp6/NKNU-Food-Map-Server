1bc241e71b28708484081ccc1d2a589599fa9f08
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-03 17:02:39 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
+// MongoDB 伺服器設定
+$dbhost = 'mongodb+srv://test:p0hFujXoDmiZovnY@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority';
+
+$dbname = 'gtwang_demo';
+
+// 連線到 MongoDB 伺服器
+$db = new MongoClient($dbhost);
+
 ?>
