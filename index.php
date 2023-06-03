8d6a17943cc4a51d2597448678a78d47e96aa140
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-03 23:23:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php 
-require 'vendor/autoload.php'; // include Composer's autoloader
+<?php
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\Query;
+// 建立 MongoDB 連接
+$manager = new Manager("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
+var_dump($manager);
 
-$client = new MongoDB\Client("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
-$database = $client->foodMap;
+$filter = ['name' => 'Hinterland'];
 
-$result = $database->restaurant->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
-
-echo "Inserted with Object ID '{$result->getInsertedId()}'";
+$query = new MongoDB\Driver\Query($filter);
+$cursor = $manager->executeQuery('foodMap.restaurant', $query); // $mongo contains the connection object to MongoDB
+foreach ($cursor as $document) {
+    var_dump($document);
+}
 ?>
