3821ef4c9ae7717f0b3df0dc9598a891804f6730
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-04 02:13:17 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,26 @@
 <?php
-use MongoDB\Driver\Client;
 
-$client = new Client("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
-$database = $client->test;
+<?php
 
-$result = $database->restaurant->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+use Exception;
+use MongoDB\Client;
+use MongoDB\Driver\ServerApi;
 
-echo "Inserted with Object ID '{$result->getInsertedId()}'";
+// Replace the placeholder with your Atlas connection string
+$uri = 'mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority';
+
+// Specify Stable API version 1
+$apiVersion = new ServerApi(ServerApi::V1);
+
+// Create a new client and connect to the server
+$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+
+try {
+    // Send a ping to confirm a successful connection
+    $client->selectDatabase('admin')->command(['ping' => 1]);
+    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+} catch (Exception $e) {
+    printf($e->getMessage());
+}
 
 ?>
