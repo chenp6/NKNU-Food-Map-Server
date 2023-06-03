bdbe96fd18179ee2a921d16c96f4d1661c586baa
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-04 04:28:40 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -18,4 +18,23 @@ try {
 }
 
 
+$collection = $client->test->users;
+
+$insertManyResult = $collection->insertMany([
+    [
+        'username' => 'admin',
+        'email' => 'admin@example.com',
+        'name' => 'Admin User',
+    ],
+    [
+        'username' => 'test',
+        'email' => 'test@example.com',
+        'name' => 'Test User',
+    ],
+]);
+
+printf("Inserted %d document(s)\n", $insertManyResult->getInsertedCount());
+
+var_dump($insertManyResult->getInsertedIds());
+
 ?>
