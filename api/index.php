a84773a8341041d77b7add8c6f7e49ffdf7d3d5a
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-04 02:23:37 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,26 +1,16 @@
 <?php
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\Query;
+// 建立 MongoDB 連接
+$manager = new Manager("mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority");
+var_dump($manager);
 
-<?php
+$filter = ['name' => 'Hinterland'];
 
-use Exception;
-use MongoDB\Client;
-use MongoDB\Driver\ServerApi;
-
-// Replace the placeholder with your Atlas connection string
-$uri = 'mongodb+srv://cpr40517:Bx4QaOJfRFn0zIpR@cluster0.9de8pzv.mongodb.net/?retryWrites=true&w=majority';
-
-// Specify Stable API version 1
-$apiVersion = new ServerApi(ServerApi::V1);
-
-// Create a new client and connect to the server
-$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
-
-try {
-    // Send a ping to confirm a successful connection
-    $client->selectDatabase('admin')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    printf($e->getMessage());
+$query = new MongoDB\Driver\Query($filter);
+$cursor = $manager->executeQuery('foodMap.restaurant', $query); // $mongo contains the connection object to MongoDB
+foreach ($cursor as $document) {
+    var_dump($document);
 }
 
 ?>
