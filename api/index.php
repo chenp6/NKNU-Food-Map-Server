df2c019630c93c7fcec1f885b5d448ee0be156c4
chenp6 <52535982+chenp6@users.noreply.github.com>
2023-06-08 13:15:13 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+
 use MongoDB\Client;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../');
+$dotenv->load();
 
 // Replace the placeholder with your Atlas connection string
 $uri = $_ENV["MONGO_URI"];
@@ -10,12 +13,32 @@ $uri = $_ENV["MONGO_URI"];
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri);
 
+
+// $places = $_GET["placeTags"];
+// $foods = $_GET["foodTags"];
+$places = ["燕巢"];
+$foods = ["晚餐"];
 try {
     // Send a ping to confirm a successful connection
-    $client->selectDatabase('admin')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+    $collection = $client->foodMap->restaurant;
+
+   $result =  $collection -> find(['$and' =>
+        [
+            [ "tags" => ['$regex'=> "字串" ]],
+            [ "tags" => ['$in'=> $places ]],
+            [ "tags"=> ['$in'=> $foods ]]
+        ]
+    ]);
+
+
+    foreach ($result as $document) {
+        var_dump($document);
+    }  
 } catch (Exception $e) {
     printf($e->getMessage());
 }
+
+
+
 
 ?>
